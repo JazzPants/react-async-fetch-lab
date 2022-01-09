5110253e98f83aee0ac9ee255e177d47c234c446
JazzPants <leighton.l14@gmail.com>
2022-01-09 18:51:57 +1100

    added notes

--- src/App.js
+++ src/App.js
@@ -12,18 +12,25 @@ class App extends Component {
 
 
   render() {
+      //react expects key for unique identifier for each child, so use array index for now
     return (
       <div>
-        {this.state.peopleInSpace.map((person, i) => <p key={i}>{i}. {person.name}</p>)}
+        {this.state.peopleInSpace.map((person, index) => <p key={index}>{index}. {person.name}</p>)}
       </div>
     )
   }
 
-  //after component mounts, fetch data
+  //after component mounts and is rendered, fetch data, and finally component updates
+  //fetch only happens once, after the first initial render.
+
+  //1. Component renders and is mounted
+  //2. Fetch (only on first render)
+  //3. Update component after fetch.
+  //4. component is in update phase (componentDidUpdate() etc.), and fetch is stopped
   componentDidMount() {
-    fetch('http://api.open-notify.org/astros.json')
-      .then(response => response.json())
-      .then(data => {
+    fetch('http://api.open-notify.org/astros.json') //other keys:values - message: success, number: 10 (people)
+      .then(response => response.json()) //translate to json
+      .then(data => { //use data and set state based on data response
         this.setState({
           peopleInSpace: data.people
         })
